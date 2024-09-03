ad78a683763f65d1f28838db2852e7f421203a4e
HeljaeRaeisaenen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2024-09-03 18:25:03 +0300

    e 10.15

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,15 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 import LogInForm from './LogInForm';
 import useSignIn from '../hooks/useSignIn';
 
 const SignIn = () => {
   const [signIn] = useSignIn();
+  const navigate = useNavigate();
 
   const onSubmit = async (values) => {
     const { username, password } = values;
 
     try {
       const { data } = await signIn({ username, password });
-      console.log(data.authenticate);
+      //console.log(data.authenticate);
+      navigate("/")
     } catch (e) {
       console.log(e);
     }
--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,13 +1,21 @@
-import { useMutation } from "@apollo/client";
+import { useMutation, useApolloClient } from "@apollo/client";
 import { AUTHENTICATE } from "../graphql/mutations";
 
+import useAuthStorage from "../hooks/useAuthStorage";
+
 const useSignIn = () => {
-  const [mutate, result] = useMutation(AUTHENTICATE);
+  const authStorage = useAuthStorage();
+  const client = useApolloClient();
+  const [authenticate, result] = useMutation(AUTHENTICATE);
 
   const signIn = async ({ username, password }) => {
-    return await mutate({
+    const { data } = await authenticate({
       variables: { credentials: { username, password } },
     });
+    await authStorage.setAccessToken(data.authenticate);
+    client.resetStore();
+
+    return data;
   };
 
   return [signIn, result];
