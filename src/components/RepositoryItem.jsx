72bb210d364bd21996f5a0c50ac82addde01f4ef
HeljaeRaeisaenen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2024-09-05 19:19:35 +0300

    e 10.19

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,6 +1,6 @@
 import { StyleSheet, View, Image, Dimensions, Pressable } from "react-native";
 import { useParams } from 'react-router-dom';
-import { useNavigate } from "react-router-dom";
+import * as Linking from 'expo-linking';
 
 import Text from "./Text";
 import theme from "./theme";
@@ -9,7 +9,9 @@ import useRepository from "../hooks/useRepository";
 const styles = StyleSheet.create({
     item: {
       display: "flex",
+      flex: 1,
       flexDirection: "column",
+      justifyContent: "space-between",
       maxWidth: Dimensions.get('window').width - 16,
       padding: 20,
       marginVertical: 4,
@@ -51,89 +53,97 @@ const styles = StyleSheet.create({
     statText: {
       fontWeight: "bold",
       fontSize: theme.fontSizes.subheading,
+    },
+    buttonLike: {
+      backgroundColor: theme.colors.accent2,
+      borderRadius: 3,
+      padding: 5,
+      margin: 5,
     }
   });
 
-const UrlButton = props => {
-  if (props.listView) return;
-  return <View>
-    <Text>this is a singel item</Text>
-  </View>
+const UrlButton = ({listView, url}) => {
+  if (listView) return;
+  return <Pressable onPress={() => Linking.openURL(url)}>
+      <View style={[styles.buttonLike, {}]}>
+        <Text style={{
+          alignSelf: "center",
+          color: theme.colors.textSecondary,
+          fontSize: theme.fontSizes.subheading,
+          }}>
+          Open in Github
+        </Text>
+      </View>
+    </Pressable>
 }
 
 const RepositoryItem = props => {
-  console.log("got repo",useRepository({id: useParams().repoId}));
-  console.log("params", useParams().repoId)
   const item = props.item ? props.item : useRepository({id: useParams().repoId}).repository;
   if (!item) return <></>
 
-  //console.log("item ", item)
-
-  return <View testID={"repositoryItem"} style={styles.item}>
+  return <View testID={"repositoryItem"} style={[
+    styles.item,
+    // This is probably horrible but I can't figure out why it displays so differently in listview
+    // and single repo view. The text was hidden, I spent hours and this is my best solution :( 
+    props.listView ? {} : {maxHeight: Dimensions.get('window').height * 0.36,}
+    ]}>
     <View style={{
       flexDirection: "row",
-      flex: 1,
       flexWrap: "nowrap",
-      alignItems: "space-between"
+      alignContent: "flex-start"
     }}>
       <Image 
         style={styles.img}
         source={{uri: item.ownerAvatarUrl}}
       />
-      <View style={{flexDirection: "column", }}>
-        <Text style={styles.title}>
+      <View style={{flexDirection: "column"}}>
+        <Text style={[styles.title, {}]}>
           {item.fullName}
         </Text>
-        <View style={styles.textArea}>
+        <View style={[styles.textArea, {minHeight: 40}]}>
           <Text style={styles.textArea}>
             &quot;{item.description}&quot;
           </Text>
         </View>
-        <View style={{
-            backgroundColor: theme.colors.accent2,
-            borderRadius: 3,
-            alignSelf: "flex-start",
-            padding: 5,
-            margin: 5,
-            }}>
-            <Text style={{color: theme.colors.textSecondary}}>
-              {item.language}
-            </Text>
-          </View>
+        <View style={[styles.buttonLike, {alignSelf: "flex-start"}]}>
+          <Text style={{color: theme.colors.textSecondary}}>
+            {item.language}
+          </Text>
         </View>
       </View>
+    </View>
         
-      <View style={styles.stats}>
-        <View style={styles.statItem}>
-          <Text style={styles.statText}>
-            {Math.round(item.forksCount / 100)/10}k
-          </Text>
-          <Text> forks
-          </Text>
-        </View>
-        <View style={styles.statItem}>
+    <View style={styles.stats}>
+      <View style={styles.statItem}>
         <Text style={styles.statText}>
-            {Math.round(item.stargazersCount / 100)/10}k
-          </Text>
-          <Text> stars
-          </Text>
-        </View>
-        <View style={styles.statItem}>
+          {Math.round(item.forksCount / 100)/10}k
+        </Text>
+        <Text> forks
+        </Text>
+      </View>
+      <View style={styles.statItem}>
         <Text style={styles.statText}>
-            {item.ratingAverage}
-          </Text>
-          <Text> rating
-          </Text>
-        </View>
-        <View style={styles.statItem}>
+          {Math.round(item.stargazersCount / 100)/10}k
+        </Text>
+        <Text> stars
+        </Text>
+      </View>
+      <View style={styles.statItem}>
         <Text style={styles.statText}>
-            {item.reviewCount}
+          {item.ratingAverage}
+        </Text>
+        <Text> rating
+        </Text>
+      </View>
+      <View style={styles.statItem}>
+        <Text style={styles.statText}>
+          {item.reviewCount}
         </Text>
         <Text> reviews
         </Text>
       </View>
     </View>
-    {UrlButton({listView: props.listView})}
+    {UrlButton({listView: props.listView, url:item.url})}
   </View>
 }
 
